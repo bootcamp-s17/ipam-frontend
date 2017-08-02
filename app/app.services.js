32557227eb4074b17f5739454efb7bf80bc60f28
Janine Hempy <jhempy@gmail.com>
2017-08-02 19:28:53 -0400

    Changes needed for Heroku deployment

--- app/app.services.js
+++ app/app.services.js
@@ -2,33 +2,33 @@
 
 function ipamService($resource) {
 	// All of the site api functions
-		let getSites = () => $resource('http://localhost:7000/api/sites/:site', {site: "@site"});
-		let	addSite = () => $resource('http://localhost:7000/api/sites');
-		let updateSite = () => $resource('http://localhost:7000/api/sites/:site', {site: "@site"}, {
+		let getSites = () => $resource('https://ipam-backend.herokuapp.com/api/sites/:site', {site: "@site"});
+		let	addSite = () => $resource('https://ipam-backend.herokuapp.com/api/sites');
+		let updateSite = () => $resource('https://ipam-backend.herokuapp.com/api/sites/:site', {site: "@site"}, {
             'update': {method: 'PUT'}
         	});
 		// console.log(updateSite().update());
 		// console.log(updateSite().update());
 	// All of the Subnet api functions
-		let	getSubnets = () => $resource('http://localhost:7000/api/subnets/:subnet', {subnet: "@subnet"});
-		let addSubnet = ()=> $resource('http://localhost:7000/api/subnets');
-		let updateSubnet = () => $resource('http://localhost:7000/api/subnets/:subnet', {subnet: "@subnet"}, 
+		let	getSubnets = () => $resource('https://ipam-backend.herokuapp.com/api/subnets/:subnet', {subnet: "@subnet"});
+		let addSubnet = ()=> $resource('https://ipam-backend.herokuapp.com/api/subnets');
+		let updateSubnet = () => $resource('https://ipam-backend.herokuapp.com/api/subnets/:subnet', {subnet: "@subnet"}, 
 			{'update': {metod: 'PUT'}
 			});
 	// All of the ip endpoint api functions
-		let getIpBySubnet =()=> $resource('http://localhost:7000/api/ip/:subnet', {subnet: "@subnet"});
-		let getNextIp = () => $resource('http://localhost:7000/api/ip/:subnet/next', {subnet: "@subnet"});
-		let checkIp = () => $resource('http://localhost:7000/api/ip/:subnet/check/:checkIp', {subnet: "@subnet", checkIp: "@checkIp"});
+		let getIpBySubnet =()=> $resource('https://ipam-backend.herokuapp.com/api/ip/:subnet', {subnet: "@subnet"});
+		let getNextIp = () => $resource('https://ipam-backend.herokuapp.com/api/ip/:subnet/next', {subnet: "@subnet"});
+		let checkIp = () => $resource('https://ipam-backend.herokuapp.com/api/ip/:subnet/check/:checkIp', {subnet: "@subnet", checkIp: "@checkIp"});
 
 	// Mac Addresss endpoints
-		let checkMac = () => $resource('http://localhost:7000/api/mac_address/:mac', {mac: "@mac"});
+		let checkMac = () => $resource('https://ipam-backend.herokuapp.com/api/mac_address/:mac', {mac: "@mac"});
 	// all of the equipment api functions
-		let getEquipments = () => $resource('http://localhost:7000/api/equipment/:equipment', {equipment: "@equipment"});
-		let addEquipment =() => $resource('http://localhost:7000/api/equipment');
-		let updateEquipment = () => $resource('http://localhost:7000/api/equipment/:equipment', {equipment: "@equipment"},
+		let getEquipments = () => $resource('https://ipam-backend.herokuapp.com/api/equipment/:equipment', {equipment: "@equipment"});
+		let addEquipment =() => $resource('https://ipam-backend.herokuapp.com/api/equipment');
+		let updateEquipment = () => $resource('https://ipam-backend.herokuapp.com/api/equipment/:equipment', {equipment: "@equipment"},
 		 {'update': {method: 'PUT'}
 			});
-		let getEquipmentTypes = () => $resource('http://localhost:7000/api/equipment_types');
+		let getEquipmentTypes = () => $resource('https://ipam-backend.herokuapp.com/api/equipment_types');
 		
 		return {
 			// SITES
